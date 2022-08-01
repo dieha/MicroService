31fa3573a7429f2911579c8951d5daf9a0128375
Peilun <pehuang@redhat.com>
2022-08-01 14:56:48 +0800

    tmp

--- src/main/java/com/redhat/lab/excepation/DataNotFountException.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.lab.excepation;
-
-public class DataNotFountException extends RuntimeException {
-
-	public DataNotFountException(String string) {
-		super(string);
-	}
-
-}
--- src/main/java/com/redhat/lab/excepation/PasswordException.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.lab.excepation;
-
-public class PasswordException extends RuntimeException {
-
-	public PasswordException(String string) {
-		super(string);
-	}
-
-}
